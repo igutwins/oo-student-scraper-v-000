c01a6ce148c5cbb5b98ee6e24a4dd2938c70ec53
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-31 17:06:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    index = Nokogiri::HTML(File.read(index_url))
 
+    students = {} 
+
+    index.css
+    
   end
 
   def self.scrape_profile_page(profile_url)
