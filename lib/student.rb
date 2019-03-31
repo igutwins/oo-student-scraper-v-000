ff299a5bc214c027ac6baaeefc74fc0d6ee96633
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-31 18:58:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       self.new(student)
-    end 
+    end
   end
 
   def add_student_attributes(attributes_hash)
